9cdbd3b45f041ac0c6f1aebcf5dd5d611d7c656b
furkankamer <furkankamer@gmail.com>
2020-02-23 17:47:47 +0300

    ios part unloaded

--- App5.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ App5.Android/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "68b5a576-fbaf-47b4-953a-cf12247e7e52",
+		"XAMARIN_BUILD_ID", "18695be9-233e-4d54-8203-7019c405e591",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
